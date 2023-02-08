f950c6dfcc1fedbdca80592f6c0e044df29c2f63
KarthikPrakash7 <K.Prakash-2@student.tudelft.nl>
2023-02-08 10:57:58 +0100

    streaming comments

--- CommentExtract.py
+++ CommentExtract.py
@@ -1,4 +1,5 @@
 import praw
+import pandas as pd
 
 reddit = praw.Reddit(client_id='nb3VtwmfQQn3J-LDko4WCg',
                      client_secret='bDO6b_86XP7E94p7-8eTJCwml_s8gA',
@@ -7,7 +8,7 @@ reddit = praw.Reddit(client_id='nb3VtwmfQQn3J-LDko4WCg',
                      user_agent='scrape_reddit'
                      )
 
-subreddit = reddit.subreddit('air pollution')
+subreddit = reddit.subreddit('airpollution')
 
 for comment in subreddit.stream.comments():
     try:
@@ -19,3 +20,23 @@ for comment in subreddit.stream.comments():
         print(comment.body)
     except praw.exceptions.PRAWException as e:
         pass
+
+topics_dict = { "title":[], \
+                "score":[], \
+                "id":[], "url":[], \
+                "comms_num": [], \
+                "created": [], \
+                "body":[]}
+
+for submission in subreddit:
+    topics_dict["title"].append(submission.title)
+    topics_dict["score"].append(submission.score)
+    topics_dict["id"].append(submission.id)
+    topics_dict["url"].append(submission.url)
+    topics_dict["comms_num"].append(submission.num_comments)
+    topics_dict["created"].append(submission.created)
+    topics_dict["body"].append(submission.selftext)
+topics_data = pd.DataFrame(topics_dict)
+
+csv_data = topics_data.to_csv('pollutiondata.csv', index=True)
+print('\nCSV String:\n', csv_data)
